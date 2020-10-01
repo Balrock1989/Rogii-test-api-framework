41aea3f2e7e04546bd64cb23bae0b7ce7a9ae95d
Дмитрий Хижов <flameon1989@yandex.ru>
2020-10-01 10:18:15 +0400

    fix HTTP intercepter

--- src/test/kotlin/com/common/Intercepter.kt
+++ src/test/kotlin/com/common/Intercepter.kt
@@ -21,13 +21,13 @@ internal class LoggingInterceptor : Interceptor {
         }
         val time = (t2 - t1) / 1e6
         if (request.method.equals("GET")) {
-            logger.info(java.lang.String.format("GET " + F_REQUEST_WITHOUT_BODY + F_RESPONSE_WITH_BODY, request.url, time, request.headers, response.code, stringifyResponseBody(bodyString)))
+            logger.info(java.lang.String.format("GET $F_REQUEST_WITHOUT_BODY$F_RESPONSE_WITH_BODY", request.url, time, request.headers, response.code, stringifyResponseBody(bodyString)))
         } else if (request.method.equals("POST")) {
-            logger.info(java.lang.String.format("POST " + F_REQUEST_WITH_BODY + F_RESPONSE_WITH_BODY, request.url, time, request.headers, stringifyRequestBody(request), response.code, stringifyResponseBody(bodyString)))
+            logger.info(java.lang.String.format("POST $F_REQUEST_WITH_BODY$F_RESPONSE_WITH_BODY", request.url, time, request.headers, stringifyRequestBody(request), response.code, stringifyResponseBody(bodyString)))
         } else if (request.method.equals("PUT")) {
-            logger.info(java.lang.String.format("PUT " + F_REQUEST_WITH_BODY + F_RESPONSE_WITH_BODY, request.url, time, request.headers, request.body.toString(), response.code, stringifyResponseBody(bodyString)))
+            logger.info(java.lang.String.format("PUT $F_REQUEST_WITH_BODY$F_RESPONSE_WITH_BODY", request.url, time, request.headers, request.body.toString(), response.code, stringifyResponseBody(bodyString)))
         } else if (request.method.equals("DELETE")) {
-            logger.info(java.lang.String.format("DELETE " + F_REQUEST_WITHOUT_BODY + F_RESPONSE_WITHOUT_BODY, request.url, time, request.headers, response.code))
+            logger.info(java.lang.String.format("DELETE $F_REQUEST_WITHOUT_BODY$F_RESPONSE_WITHOUT_BODY", request.url, time, request.headers, response.code))
         }
         return if (response.body != null) {
             val body: ResponseBody = bodyString!!.toResponseBody(contentType)
@@ -48,7 +48,7 @@ internal class LoggingInterceptor : Interceptor {
         private const val F_HEADERS = "%s"
         private const val F_RESPONSE = F_BREAK + "Response: %d"
         private const val F_BODY = "body: %s"
-        private const val F_BREAKER = F_BREAK + "-------------------------------------------" + F_BREAK
+        private const val F_BREAKER = "$F_BREAK-------------------------------------------$F_BREAK"
         private const val F_REQUEST_WITHOUT_BODY = F_URL + F_TIME + F_BREAK + F_HEADERS
         private const val F_RESPONSE_WITHOUT_BODY = F_RESPONSE + F_BREAK + F_BREAKER
         private const val F_REQUEST_WITH_BODY = F_URL + F_TIME + F_BREAK + F_HEADERS + F_BODY + F_BREAK
